193da55b57b14b308dfa78565b85917f8ea8a087
Merna Hesham <mernahesham97@yahoo.com>
2025-02-12 13:27:20 +0200

    quantized version

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from transformers import pipeline, AutoTokenizer
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 import os
+
+from optimum.onnxruntime import ORTModelForSequenceClassification
 
 app = FastAPI()
 
@@ -26,4 +28,36 @@ async def predict(text: str):
     chunks = chunk_text(text, max_length=512)
 
     results = [model(chunk) for chunk in chunks]
+    return results
+
+
+## Quantized endpoint
+## Fine-tuned approach
+
+# fined_tuned_gpt2_name = "lvwerra/gpt2-imdb" 
+# fined_tuned_gpt2 = AutoModelForSequenceClassification.from_pretrained(fined_tuned_gpt2_name)
+# tokenizer_quantized = AutoTokenizer.from_pretrained(fined_tuned_gpt2)
+
+# model_quantized = pipeline("text-classification", model=fined_tuned_gpt2, tokenizer=tokenizer_quantized)
+
+# @app.post("/predict_quantized")
+# async def predict(text: str):
+
+#     results = model_quantized(text)
+    
+#     return results
+
+## Use current BERT Quantized version
+
+model_name_q = "Intel/distilbert-base-uncased-finetuned-sst-2-english-int8-static"
+int8_model = ORTModelForSequenceClassification.from_pretrained(model_name_q)
+model_q = pipeline("text-classification", model=int8_model, tokenizer=tokenizer)
+
+@app.post("/predict_quantized")
+async def predict(text: str):
+    
+    chunks = chunk_text(text, max_length=512)
+
+    results = [model_q(chunk) for chunk in chunks]
+    
     return results
